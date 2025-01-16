a13d9126b1ee0e49db19f22e53b3e201e398614c
Ren Hoshizora <mthariqaryaputra1@gmail.com>
2025-01-16 18:35:37 +0700

    first commit

---
+++ include/db.h
@@ -0,0 +1,15 @@
+#ifndef DB_H
+#define DB_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+#endif // DB_H
---
+++ src/db.c
@@ -0,0 +1,3 @@
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+}
